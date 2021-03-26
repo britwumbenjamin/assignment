b1cde00d3acfc51234db41f4411b7429ad102825
Benjamin Britwum <britwumbenjamin>
2021-03-26 10:12:51 +0000

    assignment

---
+++ components/dashboardp/dashboardp.js
@@ -0,0 +1,44 @@
+import React from 'react'
+// import Images from '../../images/download.png'
+function Dashboardp() {
+    return (
+
+
+<div className="maincontainer">
+    <div className="left">
+<div className="brandiv">
+CREATIVE TIM
+                    <img className= "logo" src="{Images}" alt="none"/>
+</div>
+<div className="menudiv">
+
+</div>
+<div className="signout">
+
+</div>
+    </div>
+    <div className="right">
+<div className="righttop">
+
+</div>
+ <div className="rightbottom">
+<div className="cardtoprow">
+<div className="card"></div>
+<div className="card"></div>
+<div className="card"></div>
+<div className="card"></div>
+
+</div>
+<div className="cardbottomrow">
+                        <div className="card"></div>
+                        <div className="card"></div>
+                        <div className="card"></div>
+
+</div>
+ </div>
+    </div>
+</div>
+    )
+}
+
+export default Dashboardp
